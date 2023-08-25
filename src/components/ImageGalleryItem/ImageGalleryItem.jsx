30166bd6c85a80078327e6ca901244095ca317c4
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-08-25 21:40:45 +0300

    Refactored GalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,33 +1,28 @@
 import { ModalImg } from 'components/Modal/Modal';
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { GalleryItem, GalleryItemImage } from './ImageGalleryItem.styled';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({ imgUrl, imgLargeUrl, tags }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
-  openModal = () => this.setState({ isModalOpen: true });
-  closeModal = () => this.setState({ isModalOpen: false });
-
-  render() {
-    const { imgUrl, imgLargeUrl, tags } = this.props;
-
-    return (
-      <GalleryItem>
-        <GalleryItemImage
-          onClick={this.openModal}
-          src={imgUrl}
-          alt={tags}
-          width="240"
-        />
-        <ModalImg
-          largeUrl={imgLargeUrl}
-          isModalOpen={this.state.isModalOpen}
-          onClose={this.closeModal}
-        />
-      </GalleryItem>
-    );
-  }
-}
+  return (
+    <GalleryItem>
+      <GalleryItemImage
+        onClick={toggleModal}
+        src={imgUrl}
+        alt={tags}
+        width="240"
+      />
+      <ModalImg
+        largeUrl={imgLargeUrl}
+        isModalOpen={isModalOpen}
+        onClose={toggleModal}
+      />
+    </GalleryItem>
+  );
+};
