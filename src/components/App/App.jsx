c39519bfc797346a7dcf3976ba088d65d1cb7239
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-08-25 21:54:53 +0300

    App зупынывся на дидмаунт рефакторинг

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import { SearchBar } from '../SearchBar/SearchBar';
@@ -9,83 +8,44 @@ import { fetchImages } from '../../api';
 import { Loader } from '../Loader/Loader';
 
 import { MainContainer } from './App.styled';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    loading: false,
-    noResults: false,
-    totalPages: 0,
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [noResults, setNoResults] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
 
-  changeQuery = newQuery => {
-    if (newQuery === this.state.query) {
+  const changeQuery = newQuery => {
+    if (newQuery === query) {
       return Notify.failure('Please, enter search params');
     }
-
-    this.setState({
-      query: `${Date.now()}/${newQuery}`,
-      images: [],
-      page: 1,
-    });
+    setQuery(`${Date.now()}/${newQuery}`);
+    setImages([]);
+    setPage(1);
   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const newQuery = this.state.query;
-
-    if (prevQuery !== newQuery || prevState.page !== this.state.page) {
-      const normalizedQuery = newQuery.slice(newQuery.indexOf('/') + 1);
-
-      this.setState({ loading: true });
-
-      try {
-        const images = await fetchImages(normalizedQuery, this.state.page);
-
-        if (images.hits.length === 0) {
-          Notify.failure(
-            'No images have been found according to your request. Please, try again!'
-          );
-          this.setState({
-            loading: false,
-          });
-        }
-        this.setState(prevState => ({
-          images: [...prevState.images, ...images.hits],
-          loading: false,
-          totalPages: Math.ceil(images.totalHits / 12),
-        }));
-      } catch (error) {
-        console.log(error);
-        this.setState({ loading: false });
-      }
-    }
-  }
-
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-    this.setState({ loading: true });
+  const handleLoadMore = () => {
+    setPage(prevState => prevState + 1);
+    setLoading(true);
   };
 
-  render() {
-    const { images, loading, page, totalPages } = this.state;
-    return (
-      <MainContainer>
-        <div>
-          <SearchBar onSubmit={this.changeQuery} />
-        </div>
-        <div>
-          <ImageGallery images={images} />
-        </div>
-        {loading && <Loader />}
-        <div>
-          {images.length !== 0 && totalPages !== page && (
-            <Button onClick={this.handleLoadMore} />
-          )}
-        </div>
-      </MainContainer>
-    );
-  }
-}
+  return (
+    <MainContainer>
+      <div>
+        <SearchBar onSubmit={changeQuery} />
+      </div>
+      <div>
+        <ImageGallery images={images} />
+      </div>
+      {loading && <Loader />}
+      <div>
+        {images.length !== 0 && totalPages !== page && (
+          <Button onClick={handleLoadMore} />
+        )}
+      </div>
+    </MainContainer>
+  );
+};
